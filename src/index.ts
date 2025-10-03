3b37a871c14c56ea8bf70b59ffaf25021c5afba5
Adugna Olana <adugnaolana1@gmail.com>
2025-10-03 15:07:48 -0400

    Add Prisma to connect to PostgraSQL

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,29 @@
 import express, {Request,Response } from "express";
 import todoRouter from './router/todoRouter'
 import { errorHandler } from "./middleware/errorHandler";
+import { PrismaClient } from "./generated/prisma";
+
+const prisma = new PrismaClient();
+async function main() {
+    await prisma.todo_list.create({
+        data: {
+            title: "wake up in the morning",
+            complete: false
+        }
+    })
+    const todos = await prisma.todo_list.findMany();
+    console.log("Todos:", todos)
+}
+
+
+main()
+    .catch((e) => {
+        console.error(e)
+        process.exit(1)
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    })
 
 const app = express()
 
@@ -9,6 +32,7 @@ app.use(express.json())
 app.use('/todo', todoRouter)
 
 app.use(errorHandler)
+
 
 app.listen(3000, () => {
     console.log("server is on")
